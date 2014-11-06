cfcda2ddc46cbd5ea468b604dbc4f52dd3edf7a1
na0ya <uoh.org@outlook.com>
2014-11-06 14:34:09 -0800

    Initialize the project with yeoman and generator-angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tubescriptionApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tubescriptionApp
+ */
+angular.module('tubescriptionApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
